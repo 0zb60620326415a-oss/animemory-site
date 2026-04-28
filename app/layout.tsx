55ffec3fed97504b6106bfc3bae10a4c6a2fe689
0zb60620326415a-oss <0zb60620326415a@gmail.com>
2026-04-28 10:14:55 +0900

    add: Google Analytics (G-76QC1VG5VV)

--- app/layout.tsx
+++ app/layout.tsx
@@ -104,6 +104,16 @@ export default function RootLayout({
       lang="ja"
       className={`${notoSerifJP.variable} ${notoSansJP.variable} ${cormorant.variable} ${shipporiMincho.variable}`}
     >
+      <head>
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-76QC1VG5VV" />
+        <script dangerouslySetInnerHTML={{ __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-76QC1VG5VV');
+        `}} />
+      </head>
       <body>{children}</body>
     </html>
   )
